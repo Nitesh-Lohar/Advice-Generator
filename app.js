7e10a1207c59d6f82f4d8ea8acf9b18c20d59daa
Nitesh Lohar <lohar.nitesh.nl99@gmail.com>
2025-08-15 15:01:04 +0530

    template

---
+++ app.js
@@ -0,0 +1,21 @@
+const url = "https://api.adviceslip.com/advice";
+const btn = document.querySelector("#btn");
+let adviceId=document.querySelector("#advice-id")
+let advice=document.querySelector("#advice")
+
+btn.addEventListener("click", async () => {
+  let adviceObj = await genAdvice();
+  adviceId.textContent="advice #"+adviceObj.slip.id
+  advice.textContent=`"${adviceObj.slip.advice}"`;
+  console.log(advice)
+});
+
+async function genAdvice() {
+  try {
+    let res = await axios.get(url);
+    let advice = res.data;
+    return advice;
+  } catch (e) {
+    return e;
+  }
+}
